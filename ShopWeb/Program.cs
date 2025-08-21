775e3e00c520232577743367b8028af6762dfac2
Nedas <nedas2004@gmail.com>
2025-08-21 23:17:32 +0300

    Added middlware and entity framework core

--- ShopWeb/Program.cs
+++ ShopWeb/Program.cs
@@ -1,5 +1,21 @@
+using ShopWeb;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.Use(async(context,next) =>
+{
+  if(context.Request.Path == "/short")
+    {
+        await context.Response.WriteAsync("Request short-circuited");
+    }
+    else
+    {
+        await next();
+    }
+});
+
+app.UseMiddleware<Middleware>();
 
 app.MapGet("/", () => "Hello World!");
 
